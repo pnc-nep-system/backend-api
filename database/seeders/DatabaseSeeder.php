b8685429f1ed2323fa6e09541bd090c72d1815be
Chhanut-Source <chhanuth21@gmail.com>
2026-07-09 14:00:08 +0700

    feat-seed users(#34)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
             BudgetBandSeeder::class,
             TaxonomySeeder::class,
             OrganisationSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
