da80545d18fabb8315193af5d8ee971d8cfb83fa
unknown <thalita.sok@student.passerellesnumeriques.org>
2026-07-24 14:39:53 +0700

    change service

--- config/services.php
+++ config/services.php
@@ -23,7 +23,7 @@ return [
         ],
     ],
 
-'imagekit' => [
+    'imagekit' => [
         'public_key'   => env('IMAGEKIT_PUBLIC_KEY'),
         'private_key'  => env('IMAGEKIT_PRIVATE_KEY'),
         'url_endpoint' => env('IMAGEKIT_URL_ENDPOINT'),
